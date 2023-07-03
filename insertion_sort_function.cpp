4ec35221bb160cc7d835fd42877e5aeb456bf911
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-07-03 09:12:57 +0530

    Create insertion_sort_function.cpp
    just function of insertion sort

---
+++ insertion_sort_function.cpp
@@ -0,0 +1,25 @@
+
+class solution
+{
+public:
+    void insert(int arr[], int i)
+    {
+        //code here
+        // return i;
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+    }
+};
