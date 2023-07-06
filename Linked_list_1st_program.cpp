a07a0ec440cb95acb1efa199afa1292405c3f227
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-07-06 12:48:03 +0530

    Update Linked_list_1st_program.cpp

--- Linked_list_1st_program.cpp
+++ Linked_list_1st_program.cpp
@@ -19,5 +19,9 @@ int main()
     node *temp2=new node(30);
     head->next=temp1;
     temp1->next=temp2;
+    /*
+            head->next=new node(20)
+            head->next->next=new node(20)
+        */
 return 0;
 }
