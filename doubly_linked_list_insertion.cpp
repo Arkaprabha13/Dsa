66a2e5af6f4cc9c8c86ced0ec97d7a3fbd67c754
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-07-11 21:27:12 +0530

    Create doubly_linked_list_insertion.cpp

---
+++ doubly_linked_list_insertion.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+    node *prev;
+    node(int x)
+    {
+        data = x; //   galti se bhi x=data nhi karna
+        next = prev = NULL;
+    }
+};
+node *insert_at_begin(node *head, int data)
+{
+    node *temp = new node(data);
+    temp->next = head;
+    if (head == NULL)
+    {
+        head->prev = temp;
+        // temp->prev = NULL;
+    }
+    return temp;
+}
+node * insert_at_end(node*head,int data)
+{
+    node *temp=new node(data);
+    
+}
+void printlist(node *head)
+{
+    node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    node *head = new node(10);
+    node *temp1 = new node(20);
+    node *temp2 = new node(30);
+    head->next = temp1;
+    temp1->prev = head;
+    temp1->next = temp2;
+    temp2->prev = temp1;
+    printlist(head);
+
+    head = insert_at_begin(head, 5);
+    printlist(head);
+    return 0;
+}
