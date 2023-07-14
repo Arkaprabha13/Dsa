f4e75467196f2673f2fb0d96c160a2fa19938d53
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-07-14 20:26:39 +0530

    Create tree_preorder_inorder_postorder.cpp

---
+++ tree_preorder_inorder_postorder.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+void preorder(Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->key << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->key << " ";
+    }
+}
+void inorder(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout << root->key << " ";
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->right->left = new Node(40);
+    root->right->right = new Node(50);
+    /*
+            10
+    20             30
+            40            50
+*/
+
+    inorder(root);
+}
