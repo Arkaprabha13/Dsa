edc9b6b66ef94b767de82b74eb4b7e36f8a37c44
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-07-15 12:39:52 +0530

    preorder also added

--- tree_inorder_iterative.cpp
+++ tree_inorder_iterative.cpp
@@ -14,7 +14,7 @@ void inorder_iterative(node *root)
 {
     stack<node *> s;
     node *current = root;
-    while (current != NULL || !s.empty())       // or s.empty()==false
+    while (current != NULL || !s.empty()) // or s.empty()==false
     {
         while (current != NULL)
         {
@@ -27,6 +27,86 @@ void inorder_iterative(node *root)
         current = current->right;
     }
 }
+void preorder_iterative(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    stack<node *> s;
+    s.push(root);
+    while (!s.empty())
+    {
+        node *current = s.top();
+        cout << current->data << " ";
+        s.pop();
+        if (current->right)
+        {
+            s.push(current->right);
+        }
+        if (current->left)
+        {
+            s.push(current->left);
+        }
+    }
+}
+void preorder_iterative_space_efficiency(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    stack<node *> s;
+    s.push(root);
+    node *current = root;
+    while (!s.empty())
+    {
+        while (current != NULL)
+        {
+            cout << current->data << " ";
+            if (current->right != NULL)
+            {
+                s.push(current->right);
+            }
+            current = current->left;
+        }
+        if (!s.empty())
+        {
+            current = s.top();
+            s.pop();
+        }
+    }
+}
+void iterativePreorder(node *root)
+{
+    // Base Case
+    if (root == NULL)
+        return;
+
+    // Create an empty stack and push root to it
+    stack<node *> nodeStack;
+    nodeStack.push(root);
+
+    /* Pop all items one by one. Do following for every popped item
+       a) print it
+       b) push its right child
+       c) push its left child
+    Note that right child is pushed first so that left is processed first */
+    while (nodeStack.empty() == false)
+    {
+        // Pop the top item from stack and print it
+        struct node *node = nodeStack.top();
+        printf("%d ", node->data);
+        nodeStack.pop();
+
+        // Push right and left children of the popped node to stack
+        if (node->right)
+            nodeStack.push(node->right);
+        if (node->left)
+            nodeStack.push(node->left);
+    }
+}
+
 int main()
 {
 
@@ -42,8 +122,13 @@ int main()
     root->right = new node(3);
     root->left->left = new node(4);
     root->left->right = new node(5);
-
+    cout << " in order traversal -> ";
     inorder_iterative(root);
+    cout << " pree order traversal -> ";
+    // iterativePreorder(root);
+    preorder_iterative(root);
+    cout<<"\nSpace efficient solution - > ";
+    preorder_iterative_space_efficiency(root);
     return 0;
 }
 // C++ program to print inorder traversal
