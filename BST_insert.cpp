6c517d69017cd633724df40644bfffd3e2cbd0a0
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-07-16 11:09:20 +0530

    Add files via upload

---
+++ BST_insert.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+Node *insert_recursive(Node *root, int x)
+{
+    if (root == NULL)
+    {
+        return new Node(x);
+    }
+    if (root->key < x)
+    {
+        root->right = insert_recursive(root->right, x);
+    }
+    if (root->key > x)
+    {
+        root->left = insert_recursive(root->left, x);
+    }
+    return root;
+}
+Node *iterative_insert(Node *root, int x){
+    Node *temp=new Node(x);
+    Node *parent=NULL,*curr=root;
+    while(curr!=NULL){
+        parent=curr;
+        if(curr->key>x)
+            curr=curr->left;
+        else if(curr->key<x)
+            curr=curr->right;
+        else
+            return root;
+    }
+    if(parent==NULL)
+        return temp;
+    if(parent->key>x)
+        parent->left=temp;
+    else
+        parent->right=temp;
+    return root;
+} 
+void inorder(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout << root->key << " ";
+        inorder(root->right);
+    }
+}
+int main()
+{
+
+    Node *root = new Node(10);
+    root->left = new Node(5);
+    root->right = new Node(15);
+    root->right->left = new Node(12);
+    root->right->right = new Node(18);
+    int x = 20;
+
+    // root = insert_recursive(root, x);
+    root = iterative_insert(root, x);
+    inorder(root);
+}
---
+++ BST_search.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+bool recursive_search(Node *root, int x){
+    if(root==NULL)
+        return false;
+    if(root->key==x)
+        return true;
+    else if(root->key>x){
+        return recursive_search(root->left,x);
+    }else{
+        return recursive_search(root->right,x);
+    }
+} 
+bool iterative_search(Node *root, int x){
+    while(root!=NULL){
+        if(root->key==x)
+            return true;
+        else if(root->key<x)
+            root=root->right;
+        else
+            root=root->left;
+    }
+    return false;
+} 
+
+int main() {
+	
+	Node *root=new Node(15);
+	root->left=new Node(5);
+	root->left->left=new Node(3);
+	root->right=new Node(20);
+	root->right->left=new Node(18);
+	root->right->left->left=new Node(16);
+	root->right->right=new Node(80);
+	int x=16;
+	
+	if(recursive_search(root,x))
+	    cout<<"Found";
+	else
+	    cout<<"Not Found";
+	if(iterative_search(root,x))
+	    cout<<"Found";
+	else
+	    cout<<"Not Found";
+}
