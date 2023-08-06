7ccfb4f565174c53defbfb6fa8434105799a2ae2
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-08-06 17:04:51 +0530

    binary search added

--- union_of_two_unsorted_arrays.cpp
+++ union_of_two_unsorted_arrays.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Binary search for arr2[i] in arr1[]
-search(int x, int *arr, int m)
+bool binary_search(int x, int *arr, int m)
+{
+    int left = 0;
+    int right = m - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == x)
+        {
+            return true;
+        }
+        else if (arr[mid] < x)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
+bool search(int x, int *arr, int m)
 {
     for (int i = 0; i < m; i++)
     {
