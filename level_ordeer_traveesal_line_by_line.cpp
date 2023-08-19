dd75a47993aa75ee957ed1f89a60b4ae35ae3de2
Arkaprabha13 <124279312+Arkaprabha13@users.noreply.github.com>
2023-08-19 12:22:07 +0530

    Add files via upload

---
+++ level_ordeer_traveesal_line_by_line.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void printLevel(Node *root)
+{
+    if (root == NULL)
+        return;
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (q.size() > 1)
+    {
+        Node *curr = q.front();
+        q.pop();
+        if (curr == NULL)
+        {
+            cout << "\n";
+            q.push(NULL);
+            continue;
+        }
+        cout << curr->key << " ";
+        if (curr->left != NULL)
+            q.push(curr->left);
+        if (curr->right != NULL)
+            q.push(curr->right);
+    }
+}
+void print(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int count = q.size();
+        for (int i = 0; i < count; i++)
+        {
+            Node *current = q.front();
+            q.pop();
+            cout << current->key << " ";
+            if (current->left != NULL)
+            {
+                q.push(current->left);
+            }
+            if (current->right != NULL)
+            {
+                q.push(current->right);
+            }
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    root->right->left = new Node(60);
+    root->right->right = new Node(70);
+
+    printLevel(root);
+    cout << endl;
+    print(root);
+}
